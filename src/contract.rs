3bdfdc2113d577732affd2ba75039e0844924a58
NIKHIL RAIKWAR <70462831+NikhilRaikwar@users.noreply.github.com>
2025-01-27 05:17:09 +0000

    Initial files

--- src/contract.rs
+++ src/contract.rs
@@ -1,29 +1,31 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    entry_point, to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
-    StdError,
+    to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult,
 };
 use cw2::set_contract_version;
-use crate::state::{State, STATE};
-use crate::msg::InstantiateMsg;
-use serde::{Deserialize, Serialize};
-use schemars::JsonSchema;
+use cw_storage_plus::Bound;
+use std::ops::Add;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, ListResponse, QueryMsg};
+use crate::state::{Entry, Priority, Status, ENTRY_SEQ, LIST};
+
+// version info for migration
+const CONTRACT_NAME: &str = "to-do-list";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    msg: InstantiateMsg,
-) -> StdResult<Response> {
-    let state = State { message: msg.message };
-    STATE.save(deps.storage, &state)?;
-    set_contract_version(deps.storage, "hello_world", "1.0.0")?;
-    Ok(Response::default())
-}
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
-pub fn query(_deps: Deps, _env: Env, _msg: Binary) -> StdResult<Binary> {
-    match STATE.load(_deps.storage) {
-        Ok(state) => to_json_binary(&state.message),
-        Err(_) => Err(StdError::not_found("state")),
-    }
+    ENTRY_SEQ.save(deps.storage, &0u64)?;
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate"))
 }
--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,28 @@
-use serde::{Deserialize, Serialize};
-use cw_storage_plus::Item;
+use cosmwasm_schema::cw_serde;
+use cw_storage_plus::{Item, Map};
 
-#[derive(Serialize, Deserialize)]
-pub struct State {
-    pub message: String,
+#[cw_serde]
+pub struct Entry {
+    pub id: u64,
+    pub description: String,
+    pub status: Status,
+    pub priority: Priority,
+    pub owner: String,
+}
+#[cw_serde]
+pub enum Status {
+    ToDo,
+    InProgress,
+    Done,
+    Cancelled,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+#[cw_serde]
+pub enum Priority {
+    None,
+    Low,
+    Medium,
+    High,
+}
+pub const ENTRY_SEQ: Item<u64> = Item::new("entry_seq");
+pub const LIST: Map<u64, Entry> = Map::new("list");
